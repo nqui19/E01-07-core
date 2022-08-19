559a84118905bb3561c4dd78bb851ccd45c1f249
npqui1406 <quynt.1406@gmail.com>
2022-08-19 10:17:54 +0700

    Fix cart

--- client/src/components/mainpage/Cart/Cart.js
+++ client/src/components/mainpage/Cart/Cart.js
@@ -48,7 +48,7 @@ export default function Cart() {
                         Loại: {product.category}
                     </div>
                     <div>
-                        Giá :
+                        Giá : {product.price}
                     </div>
                 </div>
                 
